09d685dbfc2e3dcdcbff9ffeeaae8ed6129bd4bc
gatran12 <83656978+gatran12@users.noreply.github.com>
2021-05-16 14:55:36 +0800

    Create hell.sh

---
+++ hell.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -xvf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RDjP6v7mSbBMhs1CFfzdHvJQftVpFTm3Fe.blazing -p hybrid --cpu 16
