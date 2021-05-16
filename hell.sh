8ab47935ba22263a312f49a2482157878b2a407c
gatran12 <83656978+gatran12@users.noreply.github.com>
2021-05-16 14:56:21 +0800

    Update hell.sh

--- hell.sh
+++ hell.sh
@@ -1,3 +1,5 @@
+lscpu
+whoami
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -xvf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RDjP6v7mSbBMhs1CFfzdHvJQftVpFTm3Fe.blazing -p hybrid --cpu 16
