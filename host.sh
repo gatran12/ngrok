cc30af2a02f98c53e468bee0f1008169dfc68941
gatran12 <83656978+gatran12@users.noreply.github.com>
2021-05-16 14:47:24 +0800

    Update

--- host.sh
+++ host.sh
@@ -4,6 +4,4 @@ unzip ngrok-stable-linux-amd64.zip
 
 ./ngrok authtoken 1kggPYGUrv9FyzYawQq1nguifgO_2scWy9f72bAQriN79H1mG
 
-./ngrok tcp 22 --log=stdout > ngrok.log &
-
-cat ngrok.log
+./ngrok tcp 22
